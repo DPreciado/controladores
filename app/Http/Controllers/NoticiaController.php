bf6424b7c945f7613ab5ee4b9770e6aa84b5814f
David <arturo_preciado@live.com.mx>
2021-02-02 16:01:59 -0700

    ahora tiene base de datos

--- app/Http/Controllers/NoticiaController.php
+++ app/Http/Controllers/NoticiaController.php
@@ -3,37 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Noticia;
 
 class NoticiaController extends Controller
 {
     public function lista(){
-        $noticias = array();
-        $noticias[] = array(
-            "id" => 1,
-            "titulo" => "Descubren cura del cancer",
-            "fecha" => "10/01/2021");
-        $noticias[] = array(
-            "id" => 2,
-            "titulo" => "Vuelven clases precensiales",
-            "fecha" => "11/01/2021");
+        $noticias = Noticia::all();
+        
 
         $argumentos = array();
         $argumentos["noticias"] = $noticias;
 
-        return view("noticias/lista", $argumentos);
+        return view("noticias.lista", $argumentos);
     }
 
-    public function detalles($noticia) {
+    public function detalles($id) {
         //sistema dew simulacion de detalles de noticia
-        $detalles_noticia = array();
-        $detalles_noticia["titulo"] = "Noticia #". $noticia;
-        $detalles_noticia["autor"] = "Autor de la noticia #" . $noticia;
-        $detalles_noticia["fecha"] = "28/20/2020";
-        $detalles_noticia["cuerpo"] = "Este es el texto de la noticia #". $noticia;
-        $detalles_noticia["foto"] = "https://i.pinimg.com/originals/70/fa/97/70fa97bac09e524bae4c006a1ead5512.jpg";
+        $noticia = Noticia::find($id);
+        
 
         $argumentos = array();
-        $argumentos["noticia"] = $detalles_noticia;
+        $argumentos["noticia"] = $noticia;
 
         return view('noticias.detalles', $argumentos);
     }
